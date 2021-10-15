a057001b7d8792eb2b5ade671369447e73836468
Joshua Valladares <jdvallad@ucsc.edu>
2021-10-15 15:10:47 -0700

    push boi

---
+++ cse101/prog/prog1/List.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "List.h"
+
+// Code to implement the functions in List.h
---
+++ cse101/prog/prog1/agendaManager.c
@@ -0,0 +1,37 @@
+#include "List.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct EventObj{
+    char *name;
+    int time;
+} EventObj;
+typedef EventObj* EventPtr;
+
+
+// provided for output header formatting
+void print(ListPtr l, char *agenda_name) {
+    printf("======== %s ========\n", agenda_name);
+    // print the content of the list
+
+    printf("===== END OF %s =====\n", agenda_name);
+}
+
+int main(int argc, char **argv) {
+	// declare 2 lists
+
+
+
+
+	// accept input from the user
+	
+
+
+	// process input and modify appropriate list
+	
+
+
+
+	// be sure to release memory
+}
