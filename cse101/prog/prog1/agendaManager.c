1c11a649257c565407a62b91dba3cfd4675a240e
Joshua Valladares <jdvallad@ucsc.edu>
2021-10-17 19:33:38 -0700

    a

--- cse101/prog/prog1/agendaManager.c
+++ cse101/prog/prog1/agendaManager.c
@@ -25,7 +25,7 @@ bool dataEqual(void *a, void *b){
 }
 void dataPrinter(void *a){
   EventPtr x = (EventPtr) a;
-  printf("%d:%d %s\n",x->time / 60,x->time % 60,x->name);
+  printf("%2d:%2d %s\n",x->time / 60,x->time % 60,x->name);
   return;
 }
 void freeData(void *a){
@@ -78,30 +78,30 @@ void parseLine(char *line, char buffer[200][200], int elements, bool hasInt){
   buffer[elementInd][innerInd + 1] = '\0';
   return;
 }
-void add(ListPtr lists[2], char *names[2], char buffer[200][200]){
+void add(ListPtr lists[2], char names[2][200], char buffer[200][200]){
   int index = -1;
-  if(strcmp(names[0], "") == 0){
-    index = 0;
-  }
-  if(strcmp(names[1], "") == 0){
-    index = 1;
-  }
   if(strcmp(names[0], buffer[1]) == 0){
     index = 0;
   }
   if(strcmp(names[1], buffer[1]) == 0){
     index = 1;
   }
+  if(index == -1 && *names[0] == '\0'){
+    index = 0;
+  }
+  if(index == -1 && *names[1] == '\0'){
+    index = 1;
+  }
   if(index == -1){
    printf("ERROR: Agenda not found\n");
    return;
   }
-  names[index] = buffer[1];
+  strcpy(names[index], buffer[1]);
   EventPtr e = (EventPtr) malloc(sizeof(EventObj));
   e->name = buffer[2];
-  e->time = atoi(buffer[3]);
+  e->time = 60 * atoi(buffer[3]);
   if(lists[index]->length == 0){
-    lists[index]->head = e;
+    appendList(lists[index], e);
     return;
   }
   int ind = 0;
@@ -112,25 +112,76 @@ void add(ListPtr lists[2], char *names[2], char buffer[200][200]){
       printf("ERROR: Already an event with that name\n");
       return;
     }
+    if(tempEvent->time == e->time){
+      printf("ERROR: Already an event at that time\n");
+      return;
+    }
+    if(e->time > tempEvent->time){
+      ind++;
+    }
+    temp = temp->next;
+  }
+  insertElement(lists[index], ind, e);
+  return;
+}
+void delete(ListPtr lists[2], char names[2][200], char buffer[200][200]){
+  int index = -1;
+  if((strcmp(names[0], buffer[1]) == 0) && (strcmp(names[0],"") != 0)){
+    index = 0;
+  }
+  if((strcmp(names[1], buffer[1]) == 0) && (strcmp(names[1],"") != 0)){
+    index = 1;
+  }
+  if(index == -1){
+   printf("ERROR: Agenda not found\n");
+   return;
+  }
+  int eventIndex = -1;
+  int tempIndex = 0;
+  NodePtr temp = lists[index]->head;
+  while(temp){
+    EventPtr tempEvent = (EventPtr) temp->data;
+    if(strcmp(buffer[2], tempEvent->name) == 0){
+      eventIndex = tempIndex;
+      break;
+    }
+    tempIndex++;
+    temp=temp->next;
+  }
+  if(eventIndex == -1){
+    printf("ERROR: Event not found\n");
+    return;
+  }
+  delElement(lists[index], eventIndex);
+  if(lists[index]->length == 0){
+    *names[index] = '\0';
   }
   return;
 }
-void delete(ListPtr lists[2], char *names[2], char buffer[200][200]){
+void swap(ListPtr lists[2], char names[2][200], char buffer[200][200]){
   return;
 }
-void swap(ListPtr lists[2], char *names[2], char buffer[200][200]){
+void merge(ListPtr lists[2], char names[2][200], char buffer[200][200]){
   return;
 }
-void merge(ListPtr lists[2], char *names[2], char buffer[200][200]){
-  return;
-}
-void printProperList(ListPtr lists[2], char *names[2], char buffer[200][200]){
+void printProperList(ListPtr lists[2], char names[2][200], char buffer[200][200]){
+  int index = -1;
+  if((strcmp(names[0], buffer[1]) == 0) && (strcmp(names[0],"") != 0)){
+    index = 0;
+  }
+  if((strcmp(names[1], buffer[1]) == 0) && (strcmp(names[1],"") != 0)){
+    index = 1;
+  }
+  if(index == -1){
+   printf("ERROR: Agenda not found\n");
+   return;
+  }
+  print(lists[index], names[index]);
   return;
 }
 int main(int argc, char **argv) {
-
   ListPtr lists[2];
-  char *names[2] = {"", ""};
+  char names[2][200] = {"", ""};
   char line[200];
   lists[0] = newList(&dataEqual, &dataPrinter, &freeData);
   lists[1] = newList(&dataEqual, &dataPrinter, &freeData);
@@ -142,19 +193,19 @@ int main(int argc, char **argv) {
         add(lists, names, buffer);
         break;
       case 'D':
-        parseLine(line,buffer, 2, false);
+        parseLine(line,buffer, 3, false);
         delete(lists, names, buffer);
         break;
       case 'S':
-        parseLine(line,buffer, 3, false);
+        parseLine(line,buffer, 4, false);
         swap(lists, names, buffer);
         break;
       case 'P':
-        parseLine(line,buffer, 1, false);
+        parseLine(line,buffer, 2, false);
         printProperList(lists, names, buffer);
         break;
       case 'M':
-        parseLine(line,buffer, 2, false);
+        parseLine(line,buffer, 3, false);
         merge(lists, names, buffer);
         break;
       default:
